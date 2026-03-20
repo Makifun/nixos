e19c75042f91ca26d8c41be6d33e52c5d592acd3
Makifun <makifun@pm.me>
2026-03-20 02:08:20 +0100

    sshKeyPaths test

--- hosts/ligma/sops.nix
+++ hosts/ligma/sops.nix
@@ -1,7 +1,10 @@
 { ... }:
 {
   sops = {
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.sshKeyPaths = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/persist/etc/ssh/ssh_host_ed25519_key"
+    ];
     secrets = {
       initrd_ssh_host_ed25519_key = {
         format = "yaml";
