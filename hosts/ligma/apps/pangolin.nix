5557fba16a849ec45915f13deaced21d59ae2989
Makifun <makifun@pm.me>
2026-03-21 23:46:23 +0100

    pangolin domain FIX

--- hosts/ligma/apps/pangolin.nix
+++ hosts/ligma/apps/pangolin.nix
@@ -18,7 +18,7 @@ let
       base_domain = "makifun.se";
     };
 
-    domains."makifun.se" = {
+    domains.makifun = {
       base_domain = "makifun.se";
       cert_resolver = "letsencrypt";
       prefer_wildcard_cert = true;
