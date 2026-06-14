64f18661610d11cfbcb140fbc2e5580f9aad3167
Makifun <makifun@pm.me>
2026-06-14 21:16:41 +0200

    nixos_install add wait

--- nixos_install.sh
+++ nixos_install.sh
@@ -13,10 +13,13 @@ echo "Pre-install: Refreshing sops keys for $FLAKE_NAME"
 echo "Installing flake .#$FLAKE_NAME on $HOST"
 nix run github:nix-community/nixos-anywhere -- --flake .#$FLAKE_NAME --copy-host-keys $HOST
 
+echo "Waiting for VM to boot up (15s)"
+sleep 15
+
 echo "Unlocking LUKS partitions"
 ssh -o ConnectTimeout=10 root@$HOST -p 2222
 
-echo "Waiting for boot to finish (15s)"
+echo "Waiting for NixOS to be ready (15s)"
 sleep 15
 
 echo "Post-install: Refreshing sops keys for $FLAKE_NAME"
