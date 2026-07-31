5a4a345fa60c97be6b031cfd39096999c0a335cd
Makifun <makifun@pm.me>
2026-08-01 01:31:43 +0200

    ligma grafana OG delete

--- hosts/ligma/apps/monitoring.nix
+++ hosts/ligma/apps/monitoring.nix
@@ -212,11 +212,6 @@ in
     mode = "0444";
   };
 
-  environment.etc."grafana-dashboards/exportarr_og.json" = {
-    source = ../grafana_dashboards/exportarr_og.json;
-    mode = "0444";
-  };
-
   environment.etc."grafana-dashboards/flux-cluster.json" = {
     source = ../grafana_dashboards/flux-cluster.json;
     mode = "0444";
