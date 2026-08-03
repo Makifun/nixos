6b8758ccc09383455a1a9fa1841337e538daae6a
Makifun <makifun@pm.me>
2026-08-03 14:48:39 +0200

    storma rclone bwlimit wtf

--- hosts/storma/apps/rclone-extra.nix
+++ hosts/storma/apps/rclone-extra.nix
@@ -2,7 +2,7 @@
 {
   services.rclone-cloud.vfsCacheMaxSize = "870G";
   services.rclone-cloud.vfsCacheMinFreeSize = "15G";
-  services.rclone-cloud.bwlimit = "20M";
+  services.rclone-cloud.bwlimit = "25M";
   services.rclone-cloud.transfers = 8;
   services.rclone-cloud.bufferSize = "32M";
 }
