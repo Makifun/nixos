3a62c0f7d5da8e154e47f5f3da8c64f9acc0063d
Makifun <makifun@pm.me>
2026-08-03 19:05:27 +0200

    storma rclone 30G minfreesize

--- hosts/storma/apps/rclone-extra.nix
+++ hosts/storma/apps/rclone-extra.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   services.rclone-cloud.vfsCacheMaxSize = "870G";
-  services.rclone-cloud.vfsCacheMinFreeSize = "15G";
+  services.rclone-cloud.vfsCacheMinFreeSize = "30G";
   services.rclone-cloud.bwlimit = "25M";
   services.rclone-cloud.transfers = 20;
   services.rclone-cloud.bufferSize = "32M";
