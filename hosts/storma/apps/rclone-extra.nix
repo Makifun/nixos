505e3277600ce12e1512e2b94a27496df575515f
Makifun <makifun@pm.me>
2026-08-09 19:12:09 +0200

    rclone storma 6 transfer test

--- hosts/storma/apps/rclone-extra.nix
+++ hosts/storma/apps/rclone-extra.nix
@@ -3,7 +3,7 @@
   services.rclone-cloud.vfsCacheMaxSize = "870G";
   services.rclone-cloud.vfsCacheMinFreeSize = "30G";
   services.rclone-cloud.bwlimit = "25M";
-  services.rclone-cloud.transfers = 20;
+  services.rclone-cloud.transfers = 6;
   services.rclone-cloud.bufferSize = "32M";
   services.rclone-cloud.vfsCachePollInterval = "30s";
   # Delay upload start so the smbd write phase completes first, avoiding
