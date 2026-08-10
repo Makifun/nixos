0ea6e0bd312bb6a4a7f04170e3af843fce9f0a60
Makifun <makifun@pm.me>
2026-08-10 14:29:50 +0200

    playma plex version-1.43.3.10828-00f62d37d

--- hosts/playma/apps/plex.nix
+++ hosts/playma/apps/plex.nix
@@ -3,7 +3,7 @@ let
   hostname = config.networking.hostName;
   plexBase = "/${hostname}/${hostname}/plex";
   # renovate: datasource=docker depName=lscr.io/linuxserver/plex
-  plexTag = "1.43.3";
+  plexTag = "version-1.43.3.10828-00f62d37d";
 in
 {
   systemd.tmpfiles.rules = [
