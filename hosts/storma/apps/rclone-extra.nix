9dea6941163a3ab9798dc168d41c15bcff50329f
Makifun <makifun@pm.me>
2026-08-13 01:33:58 +0200

    storma rclone try 80M bwlimit

--- hosts/storma/apps/rclone-extra.nix
+++ hosts/storma/apps/rclone-extra.nix
@@ -2,7 +2,7 @@
 {
   services.rclone-cloud.vfsCacheMaxSize = "500G";
   services.rclone-cloud.vfsCacheMinFreeSize = "30G";
-  services.rclone-cloud.bwlimit = "25M";
+  services.rclone-cloud.bwlimit = "80M";
   services.rclone-cloud.transfers = 20;
   services.rclone-cloud.bufferSize = "128M";
   services.rclone-cloud.vfsCachePollInterval = "30s";
