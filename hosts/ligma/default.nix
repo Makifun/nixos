64072d9ae5184102d6277c58c52c90051a2a34ec
Makifun <makifun@pm.me>
2026-08-12 18:33:28 +0200

    ligma infisical temp disable

--- hosts/ligma/default.nix
+++ hosts/ligma/default.nix
@@ -21,7 +21,7 @@
     ./apps/garage.nix
     ./apps/gotify.nix
     ./apps/homepage.nix
-    ./apps/infisical.nix
+    #./apps/infisical.nix
     ./apps/loki.nix
     ./apps/monitoring.nix
     ./apps/omni.nix
